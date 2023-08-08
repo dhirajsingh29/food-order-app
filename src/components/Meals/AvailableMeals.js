b292e64ec6bcc45c2b79224b94699408f667f61a
dhiraj_slt <dhiraj.kumar@sevenlakes.com>
2023-08-09 01:21:24 +0530

    Meals | MealItems | Cart | Modals

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,55 @@
+import Card from '../UI/Card';
+import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
+
+const DUMMY_MEALS = [
+    {
+        id: 'm1',
+        name: 'Sushi',
+        description: 'Finest fish and veggies',
+        price: 450,
+    },
+    {
+        id: 'm2',
+        name: 'Schnitzel',
+        description: 'A german specialty!',
+        price: 385,
+    },
+    {
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 287,
+    },
+    {
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 300,
+    },
+];
+
+const AvailableMeals = () => {
+
+    const mealsList = DUMMY_MEALS.map(meal => (
+        <MealItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
+
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
+        </section>
+    );
+};
+
+export default AvailableMeals;
